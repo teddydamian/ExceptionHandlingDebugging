f04d54e32d2a3bc79867bef0ab2e0e5c3c90771d
teddydamian <teddy.damian@gmail.com>
2020-03-16 20:48:49 -0700

    populate

---
+++ ExceptionHandlingDebugging/ExceptionHandlingDebugging/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExceptionHandlingDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my game! Let's do some math!");
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+
+        }
+        static void StartSequence()
+        {
+            try
+            {
+                Console.WriteLine("Enter a number greater than zero");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int[] array = new int[userInput];
+                Populate(array);
+                GetSum(array);
+                GetProduct(array, GetSum(array));
+                GetQuotient(GetProduct);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"You have a Format Exception of :{ex}");
+
+            }
+            catch (OverflowException exx)
+            {
+                Console.WriteLine($"You have a Overflow Exception of :{exx}");
+
+            }
+
+        }
+        static int[] Populate(int[] pop)
+        {
+
+        }
+    }
+}
